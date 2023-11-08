fa2fb96a0f3d99229449e2555399e97e87c96e32
bootstrap <ksealy93@gmail.com>
2023-11-08 16:01:29 -0500

    Ship Class
    Created Ship class
    Created Node class
    Started GameBoard  class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import './style.css'
 
-console.log('Hello World!')
+class node{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.hit = false;
+        this.ship = false;
+    }
+    
+}
+class Ship {
+    constructor(length, type) {
+        this.length = length;
+        this.type = type;
+        this.isSunk = false;
+        this.numOfHits = 0;
+    }
 
-
-function go() {
-  return 'go'
+    hit() {
+        this.numOfHits++;
+        if (this.numOfHits === this.length) {
+            this.isSunk = true;
+        }
+    }
+    isSunk() {
+        return this.isSunk;
+    }
 }
 
+class Gameboard{
 
-console.log(go())
-
-
-module.exports = go
+}
